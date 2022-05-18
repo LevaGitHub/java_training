3380fa0a5738b093c8cfc7f9faea18a0bf7da323
LevaGitHub <lvov_oleg@list.ru>
2022-05-19 00:44:29 +0300

    Реализованы проверки путем сравнения списков в тестах групп

--- addressbook-web-tests/src/test/java/ru/stqa/addressbok/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbok/tests/GroupCreationTest.java
@@ -4,15 +4,26 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.addressbok.model.GroupData;
 
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+
 public class GroupCreationTest extends TestBase{
 
   @Test
   public void testGroupCreation() throws Exception {
-    app.getNavigationHelper().goToGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
-    app.getGroupHelper().createGroup(new GroupData("Name", null, null));
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before +1);
+      app.getNavigationHelper().goToGroupPage();
+      List<GroupData> before = app.getGroupHelper().getGroupList();
+      GroupData group = new GroupData("Name", null, null);
+      app.getGroupHelper().createGroup(group);
+      List<GroupData> after = app.getGroupHelper().getGroupList();
+      Assert.assertEquals(after.size(), before.size() + 1);
+
+      before.add(group);
+      Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+      before.sort(byId);
+      after.sort(byId);
+      Assert.assertEquals(before, after);
   }
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/addressbok/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbok/tests/GroupModificationTests.java
@@ -4,22 +4,34 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.addressbok.model.GroupData;
 
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+
 public class GroupModificationTests extends TestBase{
 
     @Test
     public void testGroupModification(){
         app.getNavigationHelper().goToGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
+
         if (! app.getGroupHelper().isThereAGroup()){
             app.getGroupHelper().createGroup(new GroupData("Name", null, null));
         }
-        app.getGroupHelper().selectGroup(before-1);
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(before.size()-1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("Name", "Header", "Footer"));
+        GroupData group = new GroupData(before.get(before.size()-1).getId(), "Name", "Header", "Footer");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before);
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size());
+        before.remove(before.size()-1);
+        before.add(group);
+        Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
     }
 
 }
