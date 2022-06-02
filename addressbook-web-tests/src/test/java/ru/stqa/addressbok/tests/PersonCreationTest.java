bf8f4c659566d1514d490380692e5d490457b13e
LevaGitHub <lvov_oleg@list.ru>
2022-06-03 00:47:03 +0300

    Добавлены провайдеры ТД для контактов

--- addressbook-web-tests/src/test/java/ru/stqa/addressbok/tests/PersonCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/addressbok/tests/PersonCreationTest.java
@@ -1,30 +1,60 @@
 package ru.stqa.addressbok.tests;
 
-import javafx.beans.property.SimpleObjectProperty;
+import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
+import org.openqa.selenium.json.TypeToken;
 import org.testng.annotations.*;
 import ru.stqa.addressbok.model.PersonData;
 import ru.stqa.addressbok.model.Persons;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
 public class PersonCreationTest extends TestBase{
 
-    @Test
-    public void testPersonCreation() throws Exception {
+    @DataProvider
+    public Iterator<Object[]> validPersonsFromXml() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/persons.xml")));
+        String xml = "";
+        String line = reader.readLine();
+        while (line != null){
+            xml += line;
+            line = reader.readLine();
+        }
+        XStream xstream = new XStream();
+        xstream.allowTypes(new Class[] {PersonData.class});
+        xstream.processAnnotations(PersonData.class);
+        List<PersonData> persons = (List<PersonData>) xstream.fromXML(xml);
+        return persons.stream().map((p) -> new Object[] {p}).collect(Collectors.toList()).iterator();
+    }
+
+    @DataProvider
+    public Iterator<Object[]> validPersonsFromJson() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/persons.json")));
+        String json = "";
+        String line = reader.readLine();
+        while (line != null){
+            json += line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List<PersonData> persons = gson.fromJson(json, new TypeToken<List<PersonData>>(){}.getType());   //List<PersonData>.class
+        return persons.stream().map((p) -> new Object[] {p}).collect(Collectors.toList()).iterator();
+
+    }
+
+    @Test(dataProvider = "validPersonsFromJson")
+    public void testPersonCreation(PersonData person) throws Exception {
         app.goTo().homePage();
         Persons before = app.person().all();
-        File photo = new File("src/test/resources/stru.png");
-        PersonData person = new PersonData()
-                .withFirstName("FirstName")
-                .withMiddleName("MiddleName")
-                .withLastName("LastName")
-                .withAddress("Address")
-                .withPhone("12345789")
-                .withEmail("test@test.test")
-                .withPhoto(photo);
         app.person().createPerson(person);
         app.goTo().homePage();
         assertThat(app.person().count(), equalTo(before.size() + 1));
@@ -33,12 +63,4 @@ public class PersonCreationTest extends TestBase{
                 before.withAdded(person.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt()))));
     }
 
-//    @Test
-//    public void testCurrentDir(){
-//        File currentDir = new File(".");
-//        System.out.println(currentDir.getAbsolutePath());
-//        File photo = new File("src/test/resources/stru.png");
-//        System.out.println(photo.getAbsolutePath());
-//        System.out.println(photo.exists());
-//    }
 }
