4a32d4af80b2c4180981ae3c9eaf9688e96c32fe
LevaGitHub <lvov_oleg@list.ru>
2022-06-20 09:43:11 +0300

    Тест получения письма через James

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -20,6 +20,7 @@ public class ApplicationManager {
     private RegistrationHelper registrationHelper;
     private FtpHelper ftp;
     private MailHelper mailHelper;
+    private JamesHelper jamesHelper;
 
     public ApplicationManager(String browser)  {
         this.browser = browser;
@@ -68,6 +69,12 @@ public class ApplicationManager {
         return mailHelper;
     }
 
+    public JamesHelper james(){
+        if (jamesHelper == null) {
+            jamesHelper = new JamesHelper(this);
+        }
+        return jamesHelper;
+    }
 
     public WebDriver getDriver() {
         if (wd == null){
